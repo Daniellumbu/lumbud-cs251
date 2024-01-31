eb92724a1b30ca6423e5d862701a5082364f1210
Daniellumbu <128616598+Daniellumbu@users.noreply.github.com>
2024-01-31 08:57:49 -0600

    Update createtables.py
    edited the file and made it cleaner

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,7 +1,4 @@
 import psycopg2
-from config import load_config
-
-
 conn = psycopg2.connect(
         host="localhost",
         port=5432,
@@ -9,14 +6,10 @@ conn = psycopg2.connect(
         user="lumbud",
         password="spider665eyebrow")
         
-cursor = conn.cursor()
-
-def create_tables():
-    """ Create tables in the PostgreSQL database"""
-    commands = (
+cur = conn.cursor()
+commands = (
         """
         CREATE Table us_cities (
-        city_id SERIAL PRIMARY KEY,
         city_name VARCHAR(255) NOT NULL,
         state_code VARCHAR(2) NOT NULL,
         population INTEGER,
@@ -30,15 +23,7 @@ def create_tables():
         state_code VARCHAR(2) NOT NULL
                 )
         """)
-    try:
-        config = load_config()
-        with psycopg2.connect(**config) as conn:
-            with conn.cursor() as cur:
-                # execute the CREATE TABLE statement
-                for command in commands:
-                    cur.execute(command)
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
+cur.execute(commands)
 
-if __name__ == '__main__':
-    create_tables()
+  
+  
