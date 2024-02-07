99872e183108c51be965e8dda648617edbe4da9b
Daniellumbu <128616598+Daniellumbu@users.noreply.github.com>
2024-02-07 09:30:09 -0600

    Update flask-test.py
    task 2

--- pythonlab/flasklab/flask-test.py
+++ pythonlab/flasklab/flask-test.py
@@ -21,6 +21,38 @@ def my_sum(word1,word2):
     sum = int(word1) + int(word2)
     return str(sum)
 
+@app.route('/pop/<word1>')
+def total_population(word1):
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="lumbud",
+        user="lumbud",
+        password="spider665eyebrow")
+
+    cur = conn.cursor()
+
+    city_name = word1
+
+    if len(city_name) == 2:
+        sql = "SELECT state_name from state_abbreviations WHERE state_code ='CA';"
+        cur.execute(sql)
+        city_name = cur.fetchone()
+   
+        sql = "SELECT SUM(population) AS total_population FROM us_cities WHERE state_code = %s;"
+        
+        cur.execute( sql, [city_name])
+    
+        row = cur.fetchone()
+        return str(row[0]) + " is the total population of " + str(city_name[0])
+    else:
+        sql = "SELECT SUM(population) AS total_population FROM us_cities WHERE state_code = %s;"
+        
+        cur.execute( sql, [city_name])
+    
+        row = cur.fetchone()
+        return str(row[0]) + " is the total population of " + city_name
+
 if __name__ == '__main__':
     my_port = 5129
     app.run(host='0.0.0.0', port = my_port) 
