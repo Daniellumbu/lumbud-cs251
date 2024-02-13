15162116d79406ee99cda56c0e9288027800ba5a
Daniellumbu <128616598+Daniellumbu@users.noreply.github.com>
2024-02-13 15:19:41 -0600

    Add files via upload

---
+++ pythonlab/flasklab/part2/part2-code.py
@@ -0,0 +1,38 @@
+import psycopg2
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("state.html")
+
+@app.route('/pop/<word1>')
+def total_population(word1):
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="lumbud",
+        user="lumbud",
+        password="spider665eyebrow")
+
+    cur = conn.cursor()
+
+    city_name = word1
+
+    sql = "SELECT state_name from state_abbreviations WHERE state_code ='CA';"
+    cur.execute(sql)
+    city_name = cur.fetchone()
+
+    sql = "SELECT SUM(population) AS total_population FROM us_cities WHERE state_code = %s;"
+    
+    cur.execute( sql, [city_name])
+
+    row = cur.fetchone()
+    return render_template("result.html", randstr = str(row[0]) + " is the total population of " + str(city_name[0]))
+    
+
+if __name__ == '__main__':
+    my_port = 5129
+    app.run(host='0.0.0.0', port = my_port) 
